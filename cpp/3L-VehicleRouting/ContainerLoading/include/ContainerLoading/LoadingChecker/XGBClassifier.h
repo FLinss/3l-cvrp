f8ec4621150907e72fa1cab5ce743760c7768a7f
mxhbmnn <maximilian.hubmann@mailbox.tu-dresden.de>
2025-08-22 10:43:44 +0200

    LR and XGB Classifier classes only have dummy values

--- cpp/3L-VehicleRouting/ContainerLoading/include/ContainerLoading/LoadingChecker/XGBClassifier.h
+++ cpp/3L-VehicleRouting/ContainerLoading/include/ContainerLoading/LoadingChecker/XGBClassifier.h
@@ -1,8 +1,6 @@
 #pragma once
 
 #include "Classifier.h"
-#include <torch/script.h>
-#include <numeric> // for std::iota
 
 
 namespace ContainerLoading {
@@ -30,22 +28,7 @@ public:
                                 const Model::Container& container) override;
 
 private:
-    
-    torch::Tensor mean_tensor;
-    torch::Tensor std_tensor;
-    torch::jit::script::Module model;
-
-
     void loadStandardScalingFromJson(const std::string& scaler_path) override;
-
-    void save_tensor_to_csv(const torch::Tensor& tensor, const int status, const float output)const; 
-
-    torch::Tensor applyStandardScaling(const torch::Tensor& input) const;
-
-    torch::Tensor extractFeatures(const std::vector<Model::Cuboid>& items,
-                                  const Collections::IdVector& route,
-                                  const Model::Container& container) const;
-
 };
 
 }  // namespace ContainerLaoding
